2d391ace44a7ed647100bda3201f821762fe64a9
Tanvi Gupta <tanvigupta129@gmail.com>
2022-02-24 02:58:42 +0530

    Commit

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -6,28 +6,45 @@ class CounterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Counter')),
-      body: BlocBuilder<CounterBloc, CounterState>(
+      appBar: AppBar(title: const Text('Counter')),
+      body: BlocConsumer<CounterBloc, CounterState>(
+        listener: (context, state) {
+          if (state.wasIncremented == true) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Incremented!'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                } else if (state.wasIncremented == false) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Decremented!'),
+                      duration: Duration(milliseconds: 300),
+                    ),
+                  );
+                }
+        },
         builder: (context, count) {
           if (count.counterValue < 0) {
             return Center(
               child: Text(
                 'Negative Value ${count.counterValue}',
-                style: TextStyle(fontSize: 24.0),
+                style: const TextStyle(fontSize: 24.0),
               ),
             );
           } else if (count.counterValue % 2 == 0) {
             return Center(
               child: Text(
                 'Even Number ${count.counterValue}',
-                style: TextStyle(fontSize: 24.0),
+                style: const TextStyle(fontSize: 24.0),
               ),
             );
           }
           return Center(
             child: Text(
               '${count.counterValue}',
-              style: TextStyle(fontSize: 24.0),
+              style: const TextStyle(fontSize: 24.0),
             ),
           );
           // child: Text(
@@ -42,17 +59,17 @@ class CounterPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.symmetric(vertical: 5.0),
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
             child: FloatingActionButton(
-              child: Icon(Icons.add),
+              child: const Icon(Icons.add),
               onPressed: () =>
                   context.read<CounterBloc>().add(CounterIncrementPressed()),
             ),
           ),
           Padding(
-            padding: EdgeInsets.symmetric(vertical: 5.0),
+            padding: const EdgeInsets.symmetric(vertical: 5.0),
             child: FloatingActionButton(
-              child: Icon(Icons.remove),
+              child: const Icon(Icons.remove),
               onPressed: () =>
                   context.read<CounterBloc>().add(CounterDecrementPressed()),
             ),
